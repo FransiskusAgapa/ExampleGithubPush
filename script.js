4d9f35ef87d3b40448f0308136ba7e537407ff71
FransXave <santhothosa@gmail.com>
2023-07-19 12:04:27 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstBtn = document.querySelector("st-btn");
+let secondBtn = document.querySelector("nd-btn");
+
+function addShade(element) {
+  element.classList.add("shade");
+}
+
+function removeShade(element) {
+  element.classList.remove("shade");
+}
